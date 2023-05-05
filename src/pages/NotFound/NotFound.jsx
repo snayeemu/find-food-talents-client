37533a3e33391e73b184eda07251e5c1e9cadc3c
snayeemu <snayeemu2@gmail.com>
2023-05-05 19:43:20 +0600

    note and 404 page updated

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,15 +3,6 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <div className="d-flex my-3">
-        <img
-          className="img-fluid w-25 mx-auto rounded shadow-lg"
-          src={
-            "https://images.unsplash.com/photo-1594322436404-5a0526db4d13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2029&q=80"
-          }
-          alt=""
-        />
-      </div>
       <div>
         <img
           className="img-fluid w-100"
